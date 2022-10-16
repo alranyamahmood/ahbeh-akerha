1a8e7538994ebc6330e2691c3558fc633652af87
user <ranyamahmood55@gmail.com>
2022-10-16 09:38:06 +0300

    updates

---
+++ README.md
@@ -0,0 +1,1 @@
+#ahbeh-akerha
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+cost app = express
+const PORT = 3000
+
+// MIDDLEWARE
+app.set('view engine', 'ejs')
+
+//ROUTES 
+
+app.listen(PORT, ()=>{
+    console.log()
+})
